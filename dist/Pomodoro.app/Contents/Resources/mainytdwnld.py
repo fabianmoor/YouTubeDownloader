7569c67c2dbd2f8332a503cfa14b1b8b351b8f37
Fabian Moor Pucar <fabianmoorpucar@Fabians-MacBook.local>
2022-10-30 02:04:00 +0200

    first commit

---
+++ dist/Pomodoro.app/Contents/Resources/mainytdwnld.py
@@ -0,0 +1,33 @@
+import rumps
+from pytube import YouTube
+import pyperclip
+
+yt = "/Users/fabianmoorpucar/PycharmProjects/BatteryTaskbar/yt.png"
+icon = "/Users/fabianmoorpucar/PycharmProjects/BatteryTaskbar/icon_white.png"
+
+
+class PomodoroApp(object):
+    def __init__(self):
+        self.app = rumps.App("YouTube Downloader", None, icon)
+
+
+
+
+
+    @rumps.clicked("Download Copied URL as MP4")
+    def youtube_dl(sender):
+        import subprocess
+        s = pyperclip.paste()
+        yt = YouTube(s)
+        stream = yt.streams.get_by_itag(140)
+        stream.download("/Users/fabianmoorpucar/Documents/Splice/sounds/packs/0 Downloaded Sounds")
+        title = yt.title
+        rumps.notification("Download Succeeded", "File saved to HDD", f"{title}")
+
+    def run(self):
+        self.app.run()
+
+
+if __name__ == '__main__':
+    app = PomodoroApp()
+    app.run()
